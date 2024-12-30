2fa5a33c4c6d244314d7689692e9f87b57b1fdb6
David <davidmillett@protonmail.com>
2024-12-30 17:10:19 +0000

    added data to components

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,6 +4,7 @@ const AboutMe = () => {
     return (
         <main>
             <h2>About Me</h2>
+            <p>Is this needed in addition to the contact?</p>
         </main>
     )
 }
--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -3,14 +3,58 @@
 const ContactMe = () => {
     return (
         <main>
-            <h3>Get in touch!</h3>
-            <p>Thank you for checking out! Send an email or find me on LinkedIn and GitHub. Or, fill in the form to contact me!</p>
+            <h2>Get in touch!</h2>
+            <p>Thank you for checking me out! Want to get in touch? Feel free to zip over an email or find me on LinkedIn and GitHub. Or, simply fill in the form to reach out!</p>
             <ul>
                 <li>Email</li>
                 <li>LinkedIn</li>
                 <li>GitHub</li>
             </ul>
-            <h3>Form</h3>
+            <h3>Say hey</h3>
+            <form>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        // value={}
+                        // onChange={}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        // value={}
+                        // onChange={}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="name">Subject</label>
+                    <input
+                        type="text"
+                        id="subject"
+                        name="subject"
+                        // value={}
+                        // onChange={}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="name">Message</label>
+                    <textarea
+                        type="text"
+                        id="message"
+                        name="message"
+                        rows="6"
+                        // value={}
+                        // onChange={}
+                    />
+                </div>
+                <button>Send</button>
+            </form>
         </main>
     )
 }
