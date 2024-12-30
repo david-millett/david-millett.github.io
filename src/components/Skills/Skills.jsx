2393b7404e01fda2e7f809227655c4f50e0f9a7e
David <davidmillett@protonmail.com>
2024-12-30 15:53:59 +0000

    added skills data

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,8 +1,35 @@
 
 
+const skillsList = [
+    {name: 'HTML', icon: ''},
+    {name: 'CSS', icon: ''},
+    {name: 'SASS', icon: ''},
+    {name: 'JavaScript', icon: ''},
+    {name: 'React.js', icon: ''},
+    {name: 'Bootstrap', icon: ''},
+    {name: 'Node.js', icon: ''},
+    {name: 'MongoDB', icon: ''},
+    {name: 'Express.js', icon: ''},
+    {name: 'Python', icon: ''},
+    {name: 'Django', icon: ''},
+    {name: 'PostgreSQL', icon: ''},
+    {name: 'npm/pip', icon: ''},
+    {name: 'Git/GitHub', icon: ''},
+]
+
 const Skills = () => {
     return (
-        <h1>Skills</h1>
+        <main>
+            <h2>Skills</h2>
+            <p>Here are some skills!</p>
+            <ul>
+                {skillsList.map(skill => {
+                    return (
+                        <li key={skill.name}>{skill.name}</li>
+                    )
+                })}
+            </ul>
+        </main>
     )
 }
 
