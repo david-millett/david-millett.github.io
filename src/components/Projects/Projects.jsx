dc5f751f8f26c5b7e2c3a893e7d6f0a016cdd38c
David <davidmillett@protonmail.com>
2024-12-30 15:27:45 +0000

    added main components

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,12 @@
+
+
+const Projects = () => {
+    return (
+        <ul>
+            <li>Project 1</li>
+            <li>Project 2</li>
+        </ul>
+    )
+}
+
+export default Projects
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,21 @@
-
+// * Components
+import AboutMe from "../../components/AboutMe/AboutMe"
+import Banner from "../../components/Banner/Banner"
+import ContactMe from "../../components/ContactMe/ContactMe"
+import Experience from "../../components/Experience/Experience"
+import Projects from "../../components/Projects/Projects"
+import Skills from "../../components/Skills/Skills"
 
 const Landing = () => {
     return (
-        <>
-            <h1>Hello!</h1>
-        </>
+        <main>
+            <Banner />
+            <Skills />
+            <Projects />
+            <Experience />
+            <AboutMe />
+            <ContactMe />
+        </main>
     )
 }
 
