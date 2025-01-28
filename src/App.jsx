d6b44a93653347080360e5b18dd302d7341575de
David <davidmillett@protonmail.com>
2025-01-28 15:02:04 +0000

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 // * Components
 import NavBar from "./components/NavBar/NavBar";
 import NavBarMobile from "./NavBar copy/NavBar";
+import Arrow from "./components/Arrow/Arrow";
 
 // * Pages
 import Landing from "./pages/Landing/Landing";
-import Arrow from "./components/Arrow/Arrow";
 
 const App = () => {
 
   const [menuOpen, setMenuOpen] = useState(false)
     
-    const handleMenuStateChange = (state) => {
-        setMenuOpen(state.isOpen)
-    }
+  const handleMenuStateChange = (state) => {
+    setMenuOpen(state.isOpen)
+  }
 
   const closeMenu = () => {
     setMenuOpen(false)
-}
+  }
 
   return (
     <>
       <NavBar />
       <NavBarMobile closeMenu={closeMenu} menuOpen={menuOpen} handleMenuStateChange={handleMenuStateChange} />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Landing />} /> */}
+      {/* </Routes> */}
+      <Landing />
       <Arrow closeMenu={closeMenu} />
     </>
   );
